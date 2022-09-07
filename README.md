40c18c7868cea451f9ddeda9492a351863102440
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2022-09-07 09:53:49 +0100

    GUA-294: Set up GOV.UK Frontend
    Following the guides at https://frontend.design-system.service.gov.uk/use-nunjucks
    and https://frontend.design-system.service.gov.uk/importing-css-assets-and-javascript

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Personal Data Store hackathon prototype
+
+## Running the prototype app
+
+1. Run `npm install` to install dependencies
+2. Run `npm run build` to build the app
+3. Run `npm start` to start the local server
+4. Visit `http://localhost:3000` in your browser
--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 let nunjucks = require('nunjucks')
+var path = require('path');
 
 var indexRouter = require('./routes/index');
 
@@ -16,10 +17,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-nunjucks.configure('views', {
-  autoescape:  true,
-  express:  app
-})
+nunjucks.configure(
+  ['src/views', 'node_modules/govuk-frontend/'], 
+  {
+    autoescape:  true,
+    express:  app
+  }
+)
+
+app.use('/assets', express.static(path.join(__dirname, '/node_modules/govuk-frontend/govuk/assets')))
 
 app.set('view engine', 'njk');
 
