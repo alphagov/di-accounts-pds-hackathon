609db888a0a32f2c873109e35eed4b80a79d55d5
Dana Cotoran <dana.cotoran@digital.cabinet-office.gov.uk>
2022-09-21 12:41:05 +0100

    Add fake 'GOVUK' vouch start pages
    Content subject to change

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ import { getPort, getSessionKeys } from "./config";
 
 import indexRouter from "./routes/index";
 import accountRouter from "./routes/account";
+import vouchRouter from "./routes/vouch";
 import loginRouter from "./routes/login";
 
 const app: express.Application = express();
@@ -41,6 +42,7 @@ app.use(
 app.use("/", indexRouter);
 app.use("/login", loginRouter);
 app.use("/account", accountRouter);
+app.use("/vouch", vouchRouter);
 
 // ============================
 // Configure Localisation
