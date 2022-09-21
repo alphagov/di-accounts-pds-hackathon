b799d4067cc8d3c1c984a37434b987c435b24e32
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2022-09-21 15:03:16 +0100

    Route pages together

--- src/routes/vouch.ts
+++ src/routes/vouch.ts
@@ -8,6 +8,8 @@ import {
     voucheeConfirmationGet,
     voucheeVoucherDetailsGet,
     voucheeDoneGet,
+    voucheeYourNamePost,
+    voucheeProvidePhotoPost,
   } from "../controllers/vouch";
   
   const router = express.Router();
@@ -15,7 +17,11 @@ import {
 router.get("/request-vouch", requestVouchGet);
 router.get("/vouch-for-someone", vouchForSomeoneGet);
 router.get("/request-vouch/your-name", voucheeYourNameGet);
+router.post("/request-vouch/your-name", voucheeYourNamePost);
+
 router.get("/request-vouch/provide-photo", voucheeProvidePhotoGet);
+router.post("/request-vouch/provide-photo", voucheeProvidePhotoPost);
+
 router.get("/request-vouch/voucher-details", voucheeVoucherDetailsGet);
 router.get("/request-vouch/confirmation", voucheeConfirmationGet);
 router.get("/request-vouch/done", voucheeDoneGet);
